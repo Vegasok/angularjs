5eed630e88e17632a87306b405824fd2ca6b8e91
Vegasok <lamps23@mail.ru>
2016-02-03 16:10:58 +0300

    changed implementation sum without controller

--- app/app.js
+++ app/app.js
@@ -1,13 +1,5 @@
 (function(){
 
-	var app = angular.module('app', []);	
+	var app = angular.module('app', []);		
 	
-	app.controller('Sum', function($scope){
-		$scope.firstNumber = "";
-		$scope.secondNumber = "";
-
-		$scope.$watch('firstNumber + secondNumber', function() {
-    		$scope.sumOfTwoNumbers = $scope.firstNumber + $scope.secondNumber;
-    	});	
-	});
 })();
