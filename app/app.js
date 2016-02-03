599e1a82aa507549c074bc2434bde5b3002e8102
Vegasok <lamps23@mail.ru>
2016-02-03 12:03:18 +0300

    replaced checking sum from external html to controller

--- app/app.js
+++ app/app.js
@@ -1,3 +1,11 @@
 (function(){
 	var app = angular.module('app', []);	
+
+	app.controller('Sum', function($scope){
+		$scope.firstNumber = "";
+		$scope.secondNumber = "";
+		$scope.sumOfTwoNumbers = function(){
+			return $scope.firstNumber + $scope.secondNumber;
+		}
+	});
 })();
