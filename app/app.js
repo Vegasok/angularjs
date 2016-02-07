11afa2f3fd33bb4cb119301ce09933e967f46452
Vegasok <lamps23@mail.ru>
2016-02-07 15:28:19 +0300

    added _.compose method

--- app/app.js
+++ app/app.js
@@ -20,8 +20,17 @@
 		$scope.firstNumber = +$routeParams.firstNumber;
 		$scope.secondNumber = +$routeParams.secondNumber;
    		
-   		$scope.changeURL = function() {
-            $location.path('/'+$scope.firstNumber+'/'+$scope.secondNumber);
+
+   		 function createPath(separator) {
+            separator = separator || '/';
+
+            return separator + $scope.firstNumber + separator + $scope.secondNumber;
+        };
+
+        function changeURL(path) {
+            $location.url(path);
         }
+
+        $scope.changeURL = _.compose(changeURL, createPath);   		
 		
 	});
