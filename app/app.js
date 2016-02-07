ae1d3a96d05ec5fb4812dc5951db943fb6edd6c8
Vegasok <lamps23@mail.ru>
2016-02-07 13:18:13 +0300

    Lesson 2:Added route #/15/23

--- app/app.js
+++ app/app.js
@@ -1,3 +1,27 @@
 	var app = angular.module('app', ['ngRoute']);		
 	
-	
+	app.config(function ($routeProvider){
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/sum.html'
+			})
+			.when('/:firstNumber/:secondNumber',{
+				templateUrl: 'views/route.html',
+				controller: 'AppCtrl'
+				
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
+
+	app.controller('AppCtrl', function($scope, $routeParams, $location) {
+
+		$scope.firstNumber = +$routeParams.firstNumber;
+		$scope.secondNumber = +$routeParams.secondNumber;
+   		
+   		$scope.changeURL = function() {
+            $location.path('/'+$scope.firstNumber+'/'+$scope.secondNumber);
+        }
+		
+	});
